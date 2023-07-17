e82f7a83aa0464983184afa35eb7ce7bd9c6fc50
Irving Arreola Palacios <irving.a789@gmail.com>
2023-07-17 13:00:40 -0500

    modified webpack config files

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -3,19 +3,21 @@ const { Module } = require("module");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
-    mode: "development",
-    devServer: {
-        port: 8082,
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: "cart",
-            filename: "remoteEntry.js",
-            exposes: {
-                './CartShow': './src/index'
-            }}),
-        new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        }),
-    ]
-}
+  mode: "development", 
+  devServer: {
+    port: 8082,
+  },
+  plugins: [
+    new ModuleFederationPlugin({ //this is used to create the remoteEntry.js file
+      name: "cart",  //this name is used in the container webpack config
+      filename: "remoteEntry.js", //default name
+      exposes: {
+        "./CartShow": "./src/index", //aliases for the files we want to expose
+      },
+      shared: ["faker"], //this is used to share dependencies between the container and the module
+    }),
+    new HtmlWebpackPlugin({ //this is used to generate the index.html file
+      template: "./public/index.html",
+    }),
+  ],
+};
